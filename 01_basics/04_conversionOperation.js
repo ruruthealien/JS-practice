357e4260411212ce3df6d9ee8b51cb74ba4fdce1
Rupanjana Majhi <rupa210works@gmail.com>
2025-03-22 16:29:11 +0000

    conversin of data types

---
+++ 01_basics/04_conversionOperation.js
@@ -0,0 +1,22 @@
+let score ="33"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNum = Number(score)
+console.log(typeof valueInNum);
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1; false = 0
+
+let isLoggedIn = "rups"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 = true; 0 = false
+// "" = false; "rups" = true
+
+let num = 33
+let stringNum = String(num)
+console.log(stringNum)
+console.log(typeof stringNum)
