f2080a800a9710028221ee4b386bac65942020d7
Rupanjana Majhi <rupa210works@gmail.com>
2025-03-22 16:40:48 +0000

    more on convrstion

--- 01_basics/04_conversionOperation.js
+++ 01_basics/04_conversionOperation.js
@@ -20,3 +20,35 @@ let num = 33
 let stringNum = String(num)
 console.log(stringNum)
 console.log(typeof stringNum)
+
+// *********************operation***********
+
+let val = 3
+let negVal = -val
+console.log(negVal)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 ="hello"
+let str2 =" world"
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1"+2) //12
+// console.log(1+"2") //12
+// console.log("1"+2+2) //122
+// console.log(1+2+"2") //32
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+let gameCounter = 100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
+console.log(gameCounter);
