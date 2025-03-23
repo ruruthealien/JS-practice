5a4570d5e33c8c0bdca2321035fa72a9e3e1364f
Rupanjana Majhi <rupa210works@gmail.com>
2025-03-23 14:27:49 +0000

    datatype summary

---
+++ 01_basics/06_data_types_summary.js
@@ -0,0 +1,29 @@
+// primitive
+// 7 types: string, number, boolean, null, undefined, symbol, bigint
+const score =100
+const scoreVal = 100.3
+
+const isLogged = false
+const outLogges = null
+let userEmail;
+//symbol
+const id = Symbol('123')
+const id2 =  Symbol('123')
+console.log(id === id2);
+
+//bigint
+const bigNum = 14864613754655n
+
+// reference/ non-primitive: arrays, objects,functions
+
+const hero = ["captaon america","spiderman","black widow"]
+
+let myObj = {
+    name:"rups",
+    age:21,
+}
+
+const func = function(){
+    console.log("hello world");
+    
+}
