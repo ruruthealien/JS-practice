e0035d07345c741079eaa2778ad6d1626c146b2f
Rupanjana Majhi <rupa210works@gmail.com>
2025-04-07 06:44:30 +0000

    for loop

---
+++ 05_iteration/01_for_loop.js
@@ -0,0 +1,19 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i; 
+//     if (element == 5) {
+//         console.log("5 is best");
+        
+//     } 
+//     console.log(element);
+// }
+
+
+for (let i = 0; i <= 10; i++) {
+  for (let j = 0; j <= 10; j++) {
+    // console.log(`inner loop value ${j} and outer loop ${i}`);
+    // console.log(i + '*' + j + '=' + i*j);
+    
+  }   
+}
