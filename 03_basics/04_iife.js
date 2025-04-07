aebc0ace8996e1bdb8efeb493fd7fe9de37ffe9b
Rupanjana Majhi <rupa210works@gmail.com>
2025-04-07 05:35:42 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expressions (IIFE)
+// to remove global scope pollution we use iife
+
+(function name() {
+    console.log("rups");
+})();
+// ()(): function then calling
+
+(() => {
+    console.log("rupanjana");
+    
+})();
